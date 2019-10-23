4ffa3d3a4dcffe6c57a750dde96d712851ff3423
renu0110 <renukabandla03@gmail.com>
2019-10-23 16:08:39 +0530

    2 files modified, 1 added

--- index.js
+++ index.js
@@ -2,9 +2,18 @@ import React, { Component } from 'react';
 import { render } from 'react-dom';
 import Hello from './Hello';
 import './style.css';
+import Cards from './Cards';
 
 class App extends Component {
+
+  state={person:[{name:'joy',email:';sdzfzd@ddf'}, 
+  {name:'joy',email:';sdzfzd@ddf'}
+  ]}
   constructor() {
+    this.state={person:[{name:'joy',email:';sdzfzd@ddf'}, 
+  {name:'joy',email:';sdzfzd@ddf'}
+  ]}
+
     super();
     this.state = {
       name: 'React'
@@ -15,6 +24,7 @@ class App extends Component {
     return (
       <div>
         <Hello name={this.state.name} />
+        <Cards name={this.state.person[0].name} email={this.state.person[1].email} />
         <p>
           Start editing to see some magic happen :)
         </p>
